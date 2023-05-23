a8ed3f24167fc26a38a8875e1db6e308380eb40d
Mertha89 <mertha@live.de>
2023-05-23 08:58:02 +0200

    fertig

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,8 +1,27 @@
 package com.cc.java;
 
 public class App {
-    
-    
+
+    public static void main(String[] args) {
+
+        /* Instanziierung Klasse --> Objekt */
+        // Cat -- Referenztyp
+        // cat -- Referenzvariable
+
+        Cat cat1 = new Cat();
+        cat1.tellYourAddress();
+        System.out.println("Blick von aussen: " + cat1);
+
+        System.out.println("------------------------");
+
+        Cat cat2 = new Cat();
+        cat2.tellYourAddress();
+        System.out.println("Blick von aussen: " + cat2);
+
+    }
+
+    public void output(String outStr) {
+        System.out.println(outStr);
+    }
 
 }
-
